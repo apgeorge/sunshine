1238e5a60190be75f63fb6675b5c1bc3ad9ebd64
aroj <apgeorge@thoughtworks.com>
2015-05-23 12:21:55 +0530

    Basic list view of strings

--- app/src/main/java/com/android/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/android/sunshine/app/MainActivityFragment.java
@@ -1,10 +1,15 @@
 package com.android.sunshine.app;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -18,6 +23,23 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        List<String> forecasts = Arrays.asList("Today - Sunny - 88/100",
+                "Tomorrow - UMBRELLAS - 88/100",
+                "Weds - GOTO BEACH!! - 88/100",
+                "Thurs - OKIE DOKIE - 88/100",
+                "Fri - NICE>>>>>>>> - 88/100",
+                "Sat - YAY - 88/100",
+                "Sun - Sunny - 88/100");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(),
+                R.layout.list_item_forecast, R.id.list_item_forecast_textview, forecasts);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(arrayAdapter);
+
+        return rootView;
     }
 }
