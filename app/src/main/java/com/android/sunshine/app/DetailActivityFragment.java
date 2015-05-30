a8a205ac1558365ab332ba45109d7134ffd0d389
aroj <apgeorge@thoughtworks.com>
2015-05-31 01:06:34 +0530

    DetailActivity shows relevant forecast

--- app/src/main/java/com/android/sunshine/app/DetailActivityFragment.java
+++ app/src/main/java/com/android/sunshine/app/DetailActivityFragment.java
@@ -1,10 +1,12 @@
 package com.android.sunshine.app;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -18,6 +20,9 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.detail_text);
+        textView.setText(getActivity().getIntent().getStringExtra(Intent.EXTRA_TEXT));
+        return rootView;
     }
 }
